03bc733aedab62c2c361e81137d5afc68c3fcce0
Darek Glowinski <dariushg32111@gmail.com>
2017-04-15 16:08:08 +0200

    add build walls

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 function Game() {
-  this.board = new Board();
+  this.players = [];
+ 
+  this.players.push(new Player('local'));
+  
+  this.board = new Board(this.players);
   this.board.initBoard();
   this.board.draw();
 
-  this.builder = new Builder(this.board);
+  this.builder = new Builder(this.board, this.players[0]);
 }
 
+function Player(type) {
+  this.type = type;
+  this.wall = [];
+  this.cannons = [];
+}
 
-function Builder(board) {
+Player.prototype.addWall = function (segment) {
+  segment.forEach(function(seg){
+    this.wall.push({row:seg.row, col:seg.col});
+  }.bind(this));
+}
+
+function Builder(board, player) {
   this.board = board;
+  this.player = player;
+  
   this.segmentFunctions = [seg1, seg2, seg3, segL, segLlarge, seg5];
   this.segmentDirection = 0; //N
 
   this.segmentFunction = this.getRandomSegment();
-
   this.registerEventHandlers();
-
-
 
 }
 Builder.prototype.getRandomSegment = function () {
@@ -26,14 +40,6 @@ Builder.prototype.getRandomSegment = function () {
 
 Builder.prototype.registerEventHandlers = function () {
 
-  //mouse over
-/*  this.board.board.forEach(function(row, rowIndex){
-    row.forEach(function(cell, colIndex){
-      
-         $(cellSelector(rowIndex, colIndex)).mouseover(this.moveSegment(rowIndex, colIndex));
-    }.bind(this)); 
-  }.bind(this));*/
-
   $('.land').mouseover(this.moveSegment.bind(this));
   $(window).mousedown(this.click.bind(this));
 
@@ -41,27 +47,33 @@ Builder.prototype.registerEventHandlers = function () {
 
 Builder.prototype.moveSegment = function(mouse) {
   var target = getMouseTarget(mouse);
-  var segment = this.segmentFunction(target.row, target.col, this.segmentDirection);
-  this.board.drawSegment(segment); 
+  this.segment = this.segmentFunction(target.row, target.col, this.segmentDirection);
+  this.board.drawSegment(this.segment); 
 }
 
 Builder.prototype.click = function(mouse) {
   var target = getMouseTarget(mouse);
   switch(mouse.which) {
     case 1: //left click
+      if(this.board.canBuildWall(this.segment)) {
+        this.player.addWall(this.segment);
+        this.board.drawWalls();
+        this.segmentFunction = this.getRandomSegment();
+        this.segment = this.segmentFunction(target.row, target.col, this.segmentDirection);
+        this.board.drawSegment(this.segment);
+      }
       break;
     case 3:
       this.segmentDirection = (this.segmentDirection + 1 + 4) % 4;  
-      var segment = this.segmentFunction(target.row, target.col, this.segmentDirection);
-      this.board.drawSegment(segment);
-      
+      this.segment = this.segmentFunction(target.row, target.col, this.segmentDirection);
+      this.board.drawSegment(this.segment);
       break;
   }
   mouse.preventDefault();
 }
 
-function Board() {
-
+function Board(players) {
+  this.players = players;
   this.loadTerrain();
 
 }
@@ -114,10 +126,47 @@ Board.prototype.draw = function () {
 
 Board.prototype.drawSegment = function (segment) {
   $(".cell").removeClass('seg-single seg-closed-up seg-closed-down seg-closed-left seg-closed-right seg-through-horizontal seg-through-vertical seg-corner-lu seg-corner-ru seg-corner-rd seg-corner-ld');
+  if(segment.some(function(seg) {
+        return seg.row < 0 || seg.row > this.rows-1 || seg.col<0 || seg.col>this.columns-1;
+      }.bind(this))) {
+    return;
+  }
   segment.forEach(function(seg){
-    $(cellSelector(seg.row, seg.col)).addClass(seg.type);
+    $(cellSelector(seg.row, seg.col)).addClass(seg.type).addClass("segment");
   });
+  if(this.canBuildWall(segment)) {
+    $(".segment").css('border-color','white');
+  } else {
+
+    $(".segment").css('border-color','red');
+  }
 };
+
+Board.prototype.drawWalls = function () {
+  this.players.forEach(function(player){
+    player.wall.forEach(function(wallSegment) {
+      $(cellSelector(wallSegment.row, wallSegment.col)).addClass("wall");
+    })
+  })
+};
+
+Board.prototype.canBuildWall = function (segment) {
+  
+  var hasWater = segment.some(function(seg){
+    return this.board[seg.row][seg.col].terrain==='water'; 
+  }.bind(this));
+
+  var hasWall = segment.some(function(seg){
+    return this.players.some(function(player){
+      return player.wall.some(function(wallSeg){
+        return wallSeg.row===seg.row && wallSeg.col===seg.col;
+      })
+    })
+  }.bind(this));
+
+  return !(hasWater || hasWall);
+};
+
 function cellSelector(row, col) {
   return  "[data-row='"+row+"'][data-column='"+col+"']";
 }
