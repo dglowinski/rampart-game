ca5d112209055ad40ae13f54081c394a22181084
Darek Glowinski <dariushg32111@gmail.com>
2017-04-18 10:01:43 +0200

    move icons info

--- js/board.js
+++ js/board.js
@@ -12,6 +12,7 @@ Board.prototype.initBoard = function () {
             .attr('data-column', colIndex));
     }
   }
+  $(".container").append('<div id="icons-author">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>')
   $('.cell').contextmenu(function() {
     return false;
 });
